89de962c36c11c708268557b6deb0dc722139904
iondriver84 <patrickhodge13@gmail.com>
2023-04-20 22:52:17 -0600

    W1 Prove 2 assignment compete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What was your grade percentage?");
+        string number = Console.ReadLine();
+        int grade = int.Parse(number);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = "B";
+
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = "C";
+
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = "D";
+
+        }
+        else
+        {
+            letter = "F";
+
+        }
+
+        Console.WriteLine($"You got an {letter}!");
+
+        if (grade > 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You'll do better next time!");
+        }
     }
 }
