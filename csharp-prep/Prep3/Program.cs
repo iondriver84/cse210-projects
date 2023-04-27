005e82ab04eee024caa0cf0352f9b11c556b1a8f
iondriver84 <patrickhodge13@gmail.com>
2023-04-27 00:17:17 -0600

    Week 2 Prep 3 Complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomgenerator = new Random();
+
+        bool answer = false;
+        
+        int magic_num = randomgenerator.Next(1, 10);
+        do{
+            Console.Write("What is your guess?");
+            string guess = Console.ReadLine();
+            int guess_num = int.Parse(guess);
+
+            if(guess_num < magic_num){
+                Console.WriteLine("Higher");
+            }
+            else if (guess_num > magic_num){
+                Console.WriteLine("Lower");
+            }
+            else if (guess_num == magic_num){
+                answer = true;
+                Console.WriteLine("You guessed correctly!.");
+            }
+        
+    } while (answer == false);
+
+
+
+
     }
 }
