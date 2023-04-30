7595586ab1dabacad461b1fc73c723ef128fb747
iondriver84 <patrickhodge13@gmail.com>
2023-04-30 17:43:56 -0600

    Prep5 complete

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome(){
+            Console.WriteLine("Welcome to the program!");
+        }
+        static string PromptUserName(){
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            return name;
+        }
+        static int PromptUserNumber(){;
+            Console.WriteLine("What is your favorite number?");
+            string fav_num = Console.ReadLine();
+            int number = int.Parse(fav_num);
+            return number;
+        }
+        static int SquareNumber(int number){
+            int squared = number * number;
+            return squared;
+        }
+        static string DisplayResult(string name, int squared){
+            Console.WriteLine($"{name}, the number you selected squareed is {squared}.");
+        }
     }
 }
